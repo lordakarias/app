76269edd8db4d55f6a02dd8f7c39d939b7a19131
unknown <lordakarias@gmail.com>
2013-11-04 15:33:54 +0100

    index change

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+         <!DOCTYPE html>
 <html>
   <head>
     <title> -- Home</title>
@@ -7,67 +7,8 @@
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
   </head>
-  <body id="home" class="home-index">
+  <body >
+  	     <div class="temp nav pull-left col-lg-3"> <?php include "_/components/php/temp-nav.php"; ?> </div>
 
-    <div class="body-header">
-      <section class="container index"> 
-        <div class="header-container">                      
-          <?php include "_/components/php/header.php"; ?>   
-        </div>   
-          
-      </section>
-    </div>
-    
-
-    <div class="body-intro">
-
-      <section class="containe">
-           <div class="intro-idea-container">    
-                   <div class="temp nav pull-left col-lg-3"> <?php include "_/components/php/temp-nav.php"; ?> </div>
-              
-             <?php include "_/components/php/intro.php"; ?>
-          </div>
-      </section>
-    </div>
-  
-
-  <div class="body-popular-idea">
-    
-      <div class="popular-idea-container row">
-              <?php include "_/components/php/popular-idea.php"; ?>
-      </div><!-- popular-idea -->
-  </div>
-
-  <div class="body-idea-area">
-      <div class="idea-area-container">
-            <?php include "_/components/php/idea-area.php"; ?>          
-      </div><!-- idea-area -->    
-  </div>
-
-  <div class="body-teams">
-    <section class="container index"> 
-        <div class="teams-container row">         
-            <?php include "_/components/php/teams.php"; ?>              
-        </div>
-    </section>
-  </div>  
-
-
-  <div class="body-idea-name">
-
-    <?php include "_/components/php/idea-name.php"; ?>                                                                
-  </div>  
-
-  
-    <div class="body-footer">
-      <section class="container index">   
-       <div class="header-container">                      
-         <?php include "_/components/php/footer.php"; ?>
-       </div>
-      </section>
-    </div>
-
-    <script src="_/js/bootstrap.js"></script>
-    <script src="_/js/myscript.js"></script>
   </body>
-</html>
+  </html>
